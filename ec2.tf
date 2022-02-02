78e18de45c19f4440fd73f0afdf2b709ca68733d
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2022-02-02 01:17:04 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "ibm_instance" {
   ami           = "ami-0e472ba40eb589f49" 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.ibm_pub_sb.id
-  key_name      = "virginie-robel"
+  key_name      = "virginia-robel"
   count = 2
   vpc_security_group_ids = [ aws_security_group.ibm_app_sg.id ]
   user_data = file("ecomm.sh")
