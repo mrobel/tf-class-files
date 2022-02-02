00996284eb32f9b419c5f3f6c57650c9ba259191
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2022-02-02 01:17:50 +0100

    Update ecomm.sh

--- ecomm.sh
+++ ecomm.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd git
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
+sudo git clone https://github.com/mrobel/ecomm.git /var/www/html
